1a576d5dbdbfc733058529ec7f935522f3f2bf64
SaraElbably-2024 <saraelbably19@gmail.com>
2026-05-06 15:33:23 +0300

    Fix: replaced masroofy folder and removed embedded git

--- masroofy/budget/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, BudgetCycle, Transaction, DailyRecord, NotificationLog
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(BudgetCycle)
-admin.site.register(Transaction)
-admin.site.register(DailyRecord)
-admin.site.register(NotificationLog)
