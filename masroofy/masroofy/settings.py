5dfb38ceeffbbedc2354c0ffe26dedfa08f98e33
YÄSÖ <jasmen.nasri2006@gmail.com>
2026-05-07 23:55:04 +0300

    fix: allowed hosts

--- masroofy/masroofy/settings.py
+++ masroofy/masroofy/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,11 +26,7 @@ SECRET_KEY = 'django-insecure-$&xs0#-_wwn$2yk1eb5o%vll)z#xkdj8s6mh^&ffpox!0owy+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'software-assignment-2.vercel.app',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,11 +74,12 @@ WSGI_APPLICATION = 'masroofy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
@@ -110,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 USE_TZ = True
+
 TIME_ZONE = 'Africa/Cairo'
 USE_I18N = True
 
