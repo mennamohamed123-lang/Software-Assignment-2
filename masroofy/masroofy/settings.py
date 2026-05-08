ae95551bbfe64fa0a6ca755d7c9561b9a3178c4d
YÄSÖ <jasmen.nasri2006@gmail.com>
2026-05-08 03:52:20 +0300

    final deployment

--- masroofy/masroofy/settings.py
+++ masroofy/masroofy/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
